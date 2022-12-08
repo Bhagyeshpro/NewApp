912e34b1aff1fcae93e2f2d9fd9d3f800ed62c58
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2022-12-07 16:52:31 -0800

    Dev:

--- App.js
+++ App.js
@@ -3,19 +3,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View style={styles.app}>
-      <Text style={{color: "#374983", fontSize: 20}}>Today's App...</Text>
+    <View>
+      <Text>App</Text>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ffff"
-  }
-})
+const styles = StyleSheet.create({})
